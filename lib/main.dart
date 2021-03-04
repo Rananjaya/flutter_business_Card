c3405922c990cc3c4eba8f315e8d9140b0e0fbe0
Rananjaya <rananjayabandara@gmail.com>
2021-03-05 01:13:36 +0530

    Learned about How to add column nect one is row

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -12,19 +14,56 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea( // safe area is used to get our widget to safearea only take the space to visibale to user
-          child: Container( // container widget just like dev in javascrpit and view in react native / if contanoner without childern it will get full area as soon as possible or if it ha childern it wiil get childer size
-            color: Colors.white,// contioner olny has one child
-            child: Text('Hello'),
-            height: 100.0, // give height and width for contionor
-            width: 100.0, // give with for contioner
-            //margin: EdgeInsets.all(20.0), // add marign for all top bottom left right to 20.0 pixels
-            // margin: EdgeInsets.symmetric(vertical: 50.0,horizontal: 10.0), // give margin  for horizontal and vertical postions
-            //margin: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 10.0) // give the indivigula vlues for each sides.
-            margin: EdgeInsets.only(left:20.0), // only give one side
-            padding: EdgeInsets.all(20.0),
+          child: Column(
+            // verticalDirection: VerticalDirection.up,
+            //verticalDirection: VerticalDirection.down,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.start,
+            //mainAxisAlignment: MainAxisAlignment.end,
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // crossAxisAlignment: CrossAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+             children: [
+               Container( // container widget just like dev in javascrpit and view in react native / if contanoner without childern it will get full area as soon as possible or if it ha childern it wiil get childer size
+                 color: Colors.white,// contioner olny has one child
+                 child: Text('Hello'),
+                 height: 100.0, // give height and width for contionor
+                 // width: double.infinity, // give with for contioner
+                  //margin: EdgeInsets.all(20.0), // add marign for all top bottom left right to 20.0 pixels
+                  // margin: EdgeInsets.symmetric(vertical: 50.0,horizontal: 10.0), // give margin  for horizontal and vertical postions
+                  //margin: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 10.0) // give the indivigula vlues for each sides.
+                 margin: EdgeInsets.only(left:20.0), // only give one side
+                 padding: EdgeInsets.all(20.0),
+               ),
+                 SizedBox(height: 20.0,),
+               Container(
+                 color: Colors.red,
+                 child: Text('Hello'),
+                 height: 100.0,
+                 // width: double.infinity,
+                 margin: EdgeInsets.only(left:20.0),
+                 padding: EdgeInsets.all(20.0),
+               ),
+               SizedBox(height: 20.0,),
+               Container(
+                 color: Colors.yellow,
+                 child: Text('Hello'),
+                 height: 100.0,
+                 // width: double.infinity,
+                 margin: EdgeInsets.only(left:20.0),
+                 padding: EdgeInsets.all(20.0),
+               ),
+               // Container(     // this is the one way to strach to cross side (virudda side ekata)
+               //   width: double.infinity,
+               //   height: 10.0,
+               // ),
+             ],
           ),
         ),
       ),
     );
   }
 }
+
