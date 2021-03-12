414f16ea5af73ba323cced71eeb3599670aee78f
Rananjaya <rananjaya@vizuamatix,com>
2021-03-12 14:47:54 +0530

    add avatr

--- lib/main.dart
+++ lib/main.dart
@@ -13,61 +13,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: SafeArea( // safe area is used to get our widget to safearea only take the space to visibale to user
-          child: Row(
-            // verticalDirection: VerticalDirection.up,
-            //verticalDirection: VerticalDirection.down,
-            // mainAxisAlignment: MainAxisAlignment.center,
-            // mainAxisAlignment: MainAxisAlignment.start,
-            //mainAxisAlignment: MainAxisAlignment.end,
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // mainAxisAlignment: MainAxisAlignment.spaceAround,
-            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            // crossAxisAlignment: CrossAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-             children: [
-               Container( // container widget just like dev in javascrpit and view in react native / if contanoner without childern it will get full area as soon as possible or if it ha childern it wiil get childer size
-                 color: Colors.white,// contioner olny has one child
-                 child: Text('Hello'),
-                 height: 100.0, // give height and width for contionor
-                 // width: double.infinity, // give with for contioner
-                  //margin: EdgeInsets.all(20.0), // add marign for all top bottom left right to 20.0 pixels
-                  // margin: EdgeInsets.symmetric(vertical: 50.0,horizontal: 10.0), // give margin  for horizontal and vertical postions
-                  //margin: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 10.0) // give the indivigula vlues for each sides.
-                 // margin: EdgeInsets.only(left:20.0), // only give one side
-                 padding: EdgeInsets.all(20.0),
-               ),
-                SizedBox(
-                 width: 10.0,
-                ),
-               Container(
-                 color: Colors.red,
-                 child: Text('Hello'),
-                 height: 100.0,
-                 // width: double.infinity,
-                 // margin: EdgeInsets.only(left:20.0),
-                 padding: EdgeInsets.all(20.0),
-               ),
-               SizedBox(
-                 width: 10.0,
-               ),
-               Container(
-                 color: Colors.yellow,
-                 child: Text('Hello'),
-                 height: 100.0,
-                 // width: double.infinity,
-                 // margin: EdgeInsets.only(left:20.0),
-                 padding: EdgeInsets.all(20.0),
-               ),
-               // Container(     // this is the one way to strach to cross side (virudda side ekata)
-               //   width: double.infinity,
-               //   height: 10.0,
-               // ),
-             ],
-          ),
-        ),
+        body: SafeArea(
+            // safe area is used to get our widget to safearea only take the space to visibale to user
+            child: Column(
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Colors.red,
+              backgroundImage: AssetImage('images/me.jpg'),
+            ),
+          ],
+        )),
       ),
     );
   }
 }
-
