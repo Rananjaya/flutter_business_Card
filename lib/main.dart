78198bf06471ab5900768b8cb79fd855bc587312
Rananjaya <rananjaya@vizuamatix,com>
2021-03-13 21:59:52 +0530

    add padding widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,80 +14,81 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            // safe area is used to get our widget to safearea only take the space to visibale to user
-            child: Column(
-          children: [
-            CircleAvatar(
-              radius: 50.0,
-              backgroundColor: Colors.red,
-              backgroundImage: AssetImage('images/me.jpg'),
-            ),
-            Text(
-              "Rananjaya Bandara",
-              style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "FLUTTER DEVELOPER",
-              style: TextStyle(
-                fontFamily: 'SourceSansPro',
-                fontSize: 20.0,
-                color: Colors.teal[100],
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.5,
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/me.jpg'),
               ),
-            ),
-            Container(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 30.0, horizontal: 25.0),
-              padding: EdgeInsets.all(15.0),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  SizedBox(
-                    width: 30.0,
-                  ),
-                  Text(
-                    '+94767219034',
-                    style: TextStyle(
-                        fontFamily: 'SourceSansPro',
-                        fontSize: 20.0,
-                        color: Colors.teal),
-                  )
-                ],
+              Text(
+                'Rananjaya Bandara',
+                style: TextStyle(
+                    fontSize: 35.0,
+                    color: Colors.white,
+                    fontFamily: 'Pacifico'),
               ),
-            ),
-            Container(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 1.0, horizontal: 25.0),
-              padding: EdgeInsets.all(15.0),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.email,
-                    color: Colors.teal,
-                  ),
-                  SizedBox(
-                    width: 30.0,
-                  ),
-                  Text(
-                    'rananjayabandara@gmail.com',
-                    style: TextStyle(
-                        fontFamily: 'SourceSansPro',
-                        fontSize: 20.0,
-                        color: Colors.teal),
-                  )
-                ],
+              SizedBox(
+                height: 10.0,
               ),
-            )
-          ],
-        )),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    fontFamily: 'SourceSansPro'),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                // padding: EdgeInsets.all(15.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      Icon(Icons.phone_android, color: Colors.teal),
+                      SizedBox(
+                        width: 60.0,
+                      ),
+                      Text(
+                        '+94767219034',
+                        style: TextStyle(
+                            color: Colors.teal,
+                            fontFamily: 'SourceSansPro',
+                            fontSize: 20.0),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                // padding: EdgeInsets.all(15.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      Icon(Icons.email, color: Colors.teal),
+                      SizedBox(
+                        width: 30.0,
+                      ),
+                      Text(
+                        'rananjayabandara@gmail.com',
+                        style: TextStyle(
+                            color: Colors.teal,
+                            fontFamily: 'SourceSansPro',
+                            fontSize: 20.0),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
